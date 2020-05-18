95d2b7a9090676e844be298241b7794c2512b0f1
Jakub Sałamun <zellanderplx@gmail.com>
2020-05-18 13:13:46 +0200

    sdf

---
+++ Projekt-Ksiegarnia/LoginScreen.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Projekt_I
+{
+    partial class LoginScreen
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_customer_id = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.button_regi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hasło:";
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(69, 71);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(100, 22);
+            this.txt_username.TabIndex = 3;
+            // 
+            // txt_customer_id
+            // 
+            this.txt_customer_id.Location = new System.Drawing.Point(69, 105);
+            this.txt_customer_id.Name = "txt_customer_id";
+            this.txt_customer_id.Size = new System.Drawing.Size(100, 22);
+            this.txt_customer_id.TabIndex = 4;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(69, 137);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(100, 22);
+            this.txt_password.TabIndex = 5;
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(69, 180);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 23);
+            this.button_login.TabIndex = 6;
+            this.button_login.Text = "Zaloguj";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // button_regi
+            // 
+            this.button_regi.Location = new System.Drawing.Point(69, 227);
+            this.button_regi.Name = "button_regi";
+            this.button_regi.Size = new System.Drawing.Size(75, 23);
+            this.button_regi.TabIndex = 7;
+            this.button_regi.Text = "Rejestracja";
+            this.button_regi.UseVisualStyleBackColor = true;
+            this.button_regi.Click += new System.EventHandler(this.button_regi_Click);
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 450);
+            this.Controls.Add(this.button_regi);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_customer_id);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginScreen";
+            this.Text = "LoginScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_customer_id;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Button button_regi;
+    }
+}
+
---
+++ Projekt-Ksiegarnia/LoginScreen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_I
+{
+    public partial class LoginScreen : Form
+    {
+        public static string username;
+        public static string username_id_lw;
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConn = new SqlConnection(@"Data Source=DESKTOP-MPTGS57\SQLEXPRESS;Initial Catalog=BookStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            try
+            {
+                if (sqlConn.State == ConnectionState.Closed)
+                    sqlConn.Open();
+                string query = "SELECT COUNT(1) FROM [dbo].[Customers] WHERE First_name=@First_name AND Customer_ID=@Customer_ID AND Customer_password=@Customer_password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlConn);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@First_name", txt_username.Text);
+                sqlCmd.Parameters.AddWithValue("@Customer_ID", txt_customer_id.Text);
+                sqlCmd.Parameters.AddWithValue("@Customer_password", txt_password.Text);
+
+                username = txt_username.Text;
+                username_id_lw = txt_customer_id.Text;
+
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Login/Hasło/ID nie prawidłowe");
+                    txt_username.Text = "";
+                    txt_customer_id.Text = "";
+                    txt_password.Text = "";
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
+        }
+
+        private void button_regi_Click(object sender, EventArgs e)
+        {
+            RegiScreen regiScreen = new RegiScreen();
+            regiScreen.Show();
+            this.Close();
+        }
+    }
+}
